ac31dbadd4b0b421da9a14a72c4d56ef2bb9bf1d
Kirti Bhosale - Network <bhosale_kirti@network.elanco.com>
2020-07-23 03:06:30 -0400

    added by kirti

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ require('dotenv').config();
 
 express.static('public')
 
+
 app.get('/', function(req, res){
  res.sendFile('F:/2700439/NodeJs/AppPages' + '/MyPage.html');
  res.send("hello world in my first");
  res.send("my first program");
 });
+
+app.get('/hello', function(req, res){
+ 
+  res.send("welcome to first program");
+  
+ });
 //app.post('/', function(req, res) {
   //  var item = req.body.userSearchInput;
     //console.log(item);
