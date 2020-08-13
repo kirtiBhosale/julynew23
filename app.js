3db714773893c11d2bbdd7247ef78a2c2515ee77
kirtiBhosale <61185694+kirtiBhosale@users.noreply.github.com>
2020-08-13 19:28:32 +0530

    Revert "Update app.js"

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
  res.send("hello world in my first");
  res.send("my first program");
 });
-consol.log("welcome to myjuly23branch devlop");
+
 app.get('/hello', function(req, res){
  
   res.send("welcome to first program");
